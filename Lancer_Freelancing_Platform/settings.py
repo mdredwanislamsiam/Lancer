28870dacbb9764eedce622ed2e90a38622b789dc
Md Redwan Islam <redwanislamsiam13@gmail.com>
2026-01-04 22:54:41 +0600

    username_reset_confirm Updated

--- Lancer_Freelancing_Platform/settings.py
+++ Lancer_Freelancing_Platform/settings.py
@@ -169,6 +169,7 @@ SIMPLE_JWT = {
 
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
